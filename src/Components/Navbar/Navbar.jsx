e80073e9e9e81ea2237be97e55a76eaa7e602a4e
opar2043 <rezonerashidopar@gmail.com>
2025-02-01 23:15:52 +0600

    my commit again

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,78 +1,96 @@
-import { FaFacebook, FaGithub, FaLinkedin} from "react-icons/fa"
-import { SiGmail } from "react-icons/si"
-import './Navbar.css'
-import { NavLink } from "react-router-dom"
-import { ToastContainer, toast } from 'react-toastify';
+import { FaFacebook, FaGithub, FaLinkedin } from "react-icons/fa";
+import { SiGmail } from "react-icons/si";
+import "./Navbar.css";
+import { NavLink } from "react-router-dom";
+import { ToastContainer, toast } from "react-toastify";
 import { PiReadCvLogoThin } from "react-icons/pi";
 
 const Navbar = () => {
-
-  function handleToast(){
+  function handleToast() {
     toast("Wow so easy!");
   }
 
   const links = (
     <>
-      <NavLink><li><a href="#">Home</a></li></NavLink>
-      <NavLink><li><a href="#about">About</a></li></NavLink>
-      <NavLink><li><a href="#contact">Contact</a></li></NavLink>
-      <NavLink><li><a href="#projects">Projects</a></li></NavLink>
+      <NavLink>
+        <li>
+          <a href="#">Home</a>
+        </li>
+      </NavLink>
+      <NavLink>
+        <li>
+          <a href="#about">About</a>
+        </li>
+      </NavLink>
+      <NavLink>
+        <li>
+          <a href="#contact">Contact</a>
+        </li>
+      </NavLink>
+      <NavLink>
+        <li>
+          <a href="#projects">Projects</a>
+        </li>
+      </NavLink>
     </>
   );
 
   return (
     <div className=" border-b-2 border-violet-900 pb-1">
-    <div className="navbar bg-transparent  ">
-  <div className="navbar-start">
-    <div className="dropdown">
-      <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className="h-5 w-5"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor">
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth="2"
-            d="M4 6h16M4 12h8m-8 6h16" />
-        </svg>
+      <div className="navbar bg-transparent  ">
+        <div className="navbar-start">
+          <div className="dropdown">
+            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h8m-8 6h16"
+                />
+              </svg>
+            </div>
+            <ul
+              tabIndex={0}
+              className="menu menu-sm dropdown-content font-semibold text-black bg-yellow-300 rounded-box z-[1] mt-3 w-52 p-2 shadow"
+            >
+              {links}
+            </ul>
+          </div>
+          <p className="md:text-3xl text-sm font-bold text-yellow-200">
+            Rijoan Rashid Opar
+          </p>
+        </div>
+        <div className="navbar-center hidden lg:flex">
+          <ul className="menu menu-horizontal pl-2 gap-1 font-semibold">
+            {links}
+          </ul>
+        </div>
+        <div className="navbar-end">
+          {/* Navbar Icons */}
+          <div className="flex flex-wrap gap-3 text-xl w-1/2">
+           
+            <a href="https://drive.google.com/file/d/172Q_e5CaXHOKCp7iiAa9B_D9n3EOyURC/view">
+              {/* Pdf Copy */}
+              {/* <a href="https://i.ibb.co.com/jZzqdZVY/Resume.jpg"> */}
+              <button
+                onClick={handleToast}
+                className="py-2 px-3 md:px-6 flex items-center gap-1 rounded-md text-sm font-semibold bg-gradient-to-r from-violet-600 to-violet-900 hover:from-yellow-300 hover:to-yellow-600 hover:text-black"
+              >
+                <PiReadCvLogoThin className="text-lg" /> Resume
+              </button>
+            </a>
+          </div>
+        </div>
       </div>
-      <ul
-        tabIndex={0}
-        className="menu menu-sm dropdown-content font-semibold text-black bg-yellow-300 rounded-box z-[1] mt-3 w-52 p-2 shadow">
-        {links}
-      </ul>
     </div>
-    <p className="md:text-3xl text-sm font-bold text-yellow-200">Rijoan Rashid Opar</p>
-  </div>
-  <div className="navbar-center hidden lg:flex">
-    <ul className="menu menu-horizontal pl-2 gap-1 font-semibold">
-      {links}
-    </ul>
-  </div>
-  <div className="navbar-end">
-   {/* Navbar Icons */}
-   <div className="flex flex-wrap gap-3 text-xl w-1/2">
-  <ToastContainer />
-<a href="https://i.ibb.co.com/ksbWfVW3/Resume-Jpg.jpg ">
-{/* Pdf Copy */}
-{/* <a href="https://i.ibb.co.com/jZzqdZVY/Resume.jpg"> */}
-<button
-    onClick={handleToast}
-    className="py-2 px-3 md:px-6 flex items-center gap-1 rounded-md text-sm font-semibold bg-gradient-to-r from-violet-600 to-violet-900 hover:from-yellow-300 hover:to-yellow-600 hover:text-black"
-  >
-    <PiReadCvLogoThin className="text-lg" /> Resume
-  </button>
-</a>
-</div>
+  );
+};
 
- 
-  </div>
-</div>
-    </div>
-  )
-}
-
-export default Navbar
+export default Navbar;
